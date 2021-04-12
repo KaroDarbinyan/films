ba0744cc70316238d405379af026ed081a084f4e
Karo <darbinyan.karo@gmail.com>
2021-04-13 02:11:19 +0400

    fix

--- src/main/java/am/imdb/films/controller/TestController.java
+++ src/main/java/am/imdb/films/controller/TestController.java
@@ -1,20 +1,10 @@
 package am.imdb.films.controller;
 
 
-import am.imdb.films.persistence.entity.GenreEntity;
 import am.imdb.films.persistence.repository.GenreRepository;
-import am.imdb.films.service.dto.GenreDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Slice;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
